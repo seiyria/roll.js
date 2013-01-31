9ccec2e8dbb0ba118bb52fbcce0afd274d02f4c8
Kyle Kemp <imnothere199@gmail.com>
2013-01-31 10:00:02 -0600

    namespacing objects

--- roll.js
+++ roll.js
@@ -1,128 +1,150 @@
 
+//functions related to operators, operands, and operations
+var Operation = {
 
-function getPrecedence(op) {
-	if("*/".indexOf(op) != -1) return 2;
-	if("-+".indexOf(op) != -1) return 1;
-}
+	//get the precedence of an operator
+	getPrecedence: function(op) {
+		if("*/".indexOf(op) != -1) return 2;
+		if("-+".indexOf(op) != -1) return 1;
+	},
 
-//allowed operators
-function isOperator(op) {
-	return "+-/*".indexOf(op) != -1;
-}
+	//allowed operators
+	isOperator: function(op) {
+		return "+-/*".indexOf(op) != -1;
+	},
 
-//valid operands
-function isOperand(op) {
-	return !/[^0-9d]/.test(op);
-}
+	//valid operands
+	isOperand: function(op) {
+		return !/[^0-9d]/.test(op);
+	},
 
-function operateOn(op, left, right) {
-	return eval(left+op+right);
-}
-
-//turn an equation into a postfix expression
-//does not support parenthesis
-function toPostfix(equation) {
-
-	var stack = new Array()
-	var retExpression = '';
-
-	var tokens = getTokens(equation);
-
-	tokens.forEach(function(token) {
-
-		if(isOperand(token)) {
-
-			//operand conversion from die to numeric
-			if(token.indexOf('d') != -1) 
-				token = roll(token);
-
-			retExpression += token + " ";
-
-		} else if(isOperator(token)){
-
-			while(stack.length > 0) {
-				var op = stack.pop();
-
-				if(getPrecedence(op) >= getPrecedence(token))
-					retExpression += op + " ";
-
-				else {
-					stack.push(op);
-					break;
-				}
-			}
-
-			stack.push(token);
-		}
-	});
-
-	while(stack.length > 0) {
-		var op = stack.pop();
-		retExpression += op + " ";
+	//run an operation (op) from left on right
+	operateOn: function(op, left, right) {
+		return eval(left+op+right);
 	}
 
-	return retExpression.trim();
-}
+};
 
+//functions related to an expression
+var Expression = {
 
-//split the equation into tokens
-function getTokens(equation) {
+	//turn an equation into a postfix expression
+	//does not support parenthesis
+	toPostfix: function (equation) {
 
-	//don't want to deal with spaces
-	equation = equation.split(" ").join("");
+		var stack = new Array()
+		var retExpression = '';
 
-	var buffer = '';
-	var tokens = new Array();
+		var tokens = Expression.getTokens(equation);
 
-	equation.split('').forEach(function(ch) {
-		if(isOperand(ch)) {
-			buffer += ch;
-		} else if(isOperator(ch)) {
-			tokens.push(buffer);
-			tokens.push(ch);
-			buffer = '';
+		tokens.forEach(function(token) {
+
+			if(Operation.isOperand(token)) {
+
+				//operand conversion from die to numeric
+				if(token.indexOf('d') != -1) 
+					token = Roller.roll(token);
+
+				retExpression += token + " ";
+
+			} else if(Operation.isOperator(token)){
+
+				while(stack.length > 0) {
+					var op = stack.pop();
+
+					if(Operation.getPrecedence(op) >= Operation.getPrecedence(token))
+						retExpression += op + " ";
+
+					else {
+						stack.push(op);
+						break;
+					}
+				}
+
+				stack.push(token);
+			}
+		});
+
+		while(stack.length > 0) {
+			var op = stack.pop();
+			retExpression += op + " ";
 		}
-	});
 
-	if(buffer != '') tokens.push(buffer);
+		return retExpression.trim();
+	},
 
-	return tokens;
-}
+	//split the equation into tokens
+	getTokens: function(equation) {
 
-function evalPostfix(postfix) {
-	var numberStack = new Array();
+		//don't want to deal with spaces
+		equation = equation.split(" ").join("");
 
-	var tokens = postfix.split(" ");
+		var buffer = '';
+		var tokens = [];
 
-	tokens.forEach(function(token) {
+		equation.split('').forEach(function(ch) {
+			if(Operation.isOperand(ch)) {
+				buffer += ch;
+			} else if(Operation.isOperator(ch)) {
+				tokens.push(buffer);
+				tokens.push(ch);
+				buffer = '';
+			}
+		});
 
-		if(isOperand(token)) {
-			numberStack.push(token);
+		if(buffer != '') tokens.push(buffer);
 
-		} else if(isOperator(token)){
-			var right = numberStack.pop();
-			var left = numberStack.pop();
-			numberStack.push(operateOn(token, left, right));
-		}
-	});
+		return tokens;
+	},
 
-	return numberStack.pop();
-}
+	//evaluate a postfix expression
+	evalPostfix: function (postfix) {
+		var numberStack = [];
 
-function evalExpression(expression) {
-	var postfix = toPostfix(expression);
-	return evalPostfix(postfix);
-}
+		var tokens = postfix.split(" ");
 
-function rollExpression(expression) {
-	return evalExpression(expression);
-}
+		tokens.forEach(function(token) {
 
-//roll a die
-function roll(string) {
-	var xdy = string.split("d");
-	var x = parseInt(xdy[0]);
-	var y = parseInt(xdy[1]);
-	if(x <= 0 || y <= 0) return 0;
-	return Math.round(Math.random()*((x*y)-x))+x;
-}
+			if(Operation.isOperand(token)) {
+				numberStack.push(token);
+
+			} else if(Operation.isOperator(token)){
+				var right = numberStack.pop();
+				var left = numberStack.pop();
+				numberStack.push(Operation.operateOn(token, left, right));
+			}
+		});
+
+		return numberStack.pop();
+	}
+
+};
+
+//all functions related to rolling
+var Roller = {
+
+	//evaluate an expression
+	evalExpression: function(expression) {
+		var postfix = Expression.toPostfix(expression);
+		return Expression.evalPostfix(postfix);
+	},
+
+	//wrapper for evalExpression
+	rollExpression: function(expression) {
+		return Roller.evalExpression(expression);
+	},
+
+	//roll a die
+	roll: function(string) {
+		var xdy = string.split("d");
+		var x = parseInt(xdy[0]);
+		var y = parseInt(xdy[1]);
+		if(x <= 0 || y <= 0) return 0;
+		return Math.round(Math.random()*((x*y)-x))+x;
+	}
+};
+
+
+
+
+
